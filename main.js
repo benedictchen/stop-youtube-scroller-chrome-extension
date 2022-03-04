765ccd417efb3031bfcc304aabd11bab78f408e7
Benedict Chen <benedict@benedictchen.com>
2022-03-03 22:12:59 -0800

    Adding initial files.

---
+++ main.js
@@ -0,0 +1,18 @@
+function freshNode(oldElement) {
+	let newElement = oldElement.cloneNode(true);
+	oldElement.parentNode.replaceChild(newElement, oldElement);
+}
+
+if (/youtube\.com/.test(window.location.hostname)) {
+	console.log("YouTube detected");
+	window.document.body.addEventListener('yt-navigate-finish', () => {
+		setTimeout(() => {
+			Array.from(window.document.body.querySelectorAll('video')).forEach((element) => {
+				freshNode(element);
+			});	
+		}, 1000);
+	});
+} else {
+	console.log('Not YouTube.');
+}
+

374f082a3ac2e2b948e495768e7802b553f9eb7a
Benedict Chen <benedict@benedictchen.com>
2022-03-03 22:41:22 -0800

    Add initial load.

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ function freshNode(oldElement) {
 
 if (/youtube\.com/.test(window.location.hostname)) {
 	console.log("YouTube detected");
+	
+	Array.from(window.document.body.querySelectorAll('video')).forEach((element) => {
+		console.log({element})
+		freshNode(element);
+	});	
+	
 	window.document.body.addEventListener('yt-navigate-finish', () => {
 		setTimeout(() => {
 			Array.from(window.document.body.querySelectorAll('video')).forEach((element) => {
